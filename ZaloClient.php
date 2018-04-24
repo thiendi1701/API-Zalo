97193da3365b6aec081f8806b1d64c23157488d3
Duy.N <vanduy1309@gmail.com>
2018-04-25 00:58:00 +0700

    Send Image Message

--- ZaloClient.php
+++ ZaloClient.php
@@ -3,6 +3,8 @@
 use Zalo\Zalo;
 use Zalo\ZaloEndpoint;
 use Zalo\ZaloConfig;
+use Zalo\FileUpload\ZaloFile;
+
 
 class ZaloClient {
 
@@ -15,6 +17,28 @@ class ZaloClient {
         );
         $params = ['data' => $data];
         $response = $zalo->post(ZaloEndpoint::API_OA_SEND_TEXT_MSG, $params);
+        $result = $response->getDecodedBody(); // result
+        print_r($result);
+    }
+    public function SendMessImage()
+    {
+        $zalo = new Zalo(ZaloConfig::getInstance()->getConfig());
+        $data = array(
+        'uid' => "8769581709013811958", // user id
+        'imageid' => 'a5cc74fd68f981a7d8e8',
+        'message' => 'hello'
+        );
+        $params = ['data' => $data];
+        $response = $zalo->post(ZaloEndpoint::API_OA_SEND_PHOTO_MSG, $params);
+        $result = $response->getDecodedBody(); // result
+        print_r($result);
+    }
+    public function UploadImage()
+    {
+        $zalo = new Zalo(ZaloConfig::getInstance()->getConfig());
+        $filePath = 'https://www.hindustantimes.com/rf/image_size_960x540/HT/p2/2018/04/23/Pictures/_0e53a530-46d9-11e8-b38d-ae9d3b5e5930.jpg';
+        $params = ['file' => new ZaloFile($filePath)];
+        $response = $zalo->post(ZaloEndpoint::API_OA_UPLOAD_PHOTO, $params);
         $result = $response->getDecodedBody(); // result
         print_r($result);
     }
--- index.php
+++ index.php
@@ -1,34 +1,10 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 include 'ZaloClient.php';
-// use Zalo\Zalo;
-// use Zalo\ZaloConfig;
-// use Zalo\ZaloEndpoint;
-
-// $zalo = new Zalo(ZaloConfig::getInstance()->getConfig());
-// $data = array(
-//     'uid' => 4829905128097954001, // user id
-//     'message' => 'HELLO'
-// );
-// $params = ['data' => $data];
-// $response = $zalo->post(ZaloEndpoint::API_OA_SEND_TEXT_MSG, $params);
-// $result = $response->getDecodedBody(); // result
-// print_r($result);
-/** config your app id here */
-// const ZALO_APP_ID_CFG = "932541833157921742";
-
-// /** config your app secret key here */
-// const ZALO_APP_SECRET_KEY_CFG = "B1KZ45UD3yLViBiEvQcD";
-
-// * config your offical account id here 
-// const ZALO_OA_ID_CFG = "2184792249474812865";
-
-// /** config your offical account secret key here */
-// const ZALO_OA_SECRET_KEY_CFG = "2G6n9VBRQiNJiQepOLgL";
 
 $zaloClient = new ZaloClient();
-
-$zaloClient->SendMess();
-
+// $zaloClient->SendMess();
+$zaloClient->SendMessImage();
+$zaloClient->UploadImage();
 
 ?>
